44ac1e67c7142a7f3cbc108a488a5fa1a7abcea2
akhil970 <akhilchukkala999@gmail.com>
2021-08-25 16:33:25 +0530

    added mvc controllers

---
+++ IPLManagementSystemMVC/Controllers/UserMVCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPLManagementSystemMVC.Controllers
+{
+    public class UserMVCController : Controller
+    {
+        // GET: UserMVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
